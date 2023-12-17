59d63d854cf109d506b4d6459099d62b8b2e42c9
Jowie <joelestepla@gmail.com>
2023-12-17 20:32:40 +0100

    cleaning trash

--- call_registry.cpp
+++ call_registry.cpp
@@ -3,15 +3,6 @@
 /* ##IMPLEMENTACIÓ DELS MÈTODES PRIVATS## */
 //---------------------------------------------------------------------------------------------//
 
-call_registry::call_registry(nat m)
-{
-    _M = m;
-    _taula = new node_hash*[_M];
-    for (nat i = 0; i < _M; ++i) {
-        _taula[i] = NULL;
-    }
-}
-
 // Constructor de nodo tipo hash
 call_registry::node_hash::node_hash(const phone &k, node_hash *seg) throw() : _k(k), _seg(seg)
 {
@@ -413,56 +404,6 @@ void call_registry::dump(vector<phone>& V) const throw(error)
         throw error(ErrNomRepetit);
     }
 }
-
-
-/*
-void merge(vector<int>& V, int l, int m, int r) {
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 = r - m;
-
-    vector<int> L(n1), R(n2);
-
-    for (i = 0; i < n1; i++)
-        L[i] = V[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = V[m + 1 + j];
-
-    i = 0;  j = 0;  k = l;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            V[k] = L[i];
-            i++;
-        }
-        else {
-            V[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i < n1) {
-        V[k] = L[i];
-        i++;
-        k++;
-    }
-
-    while (j < n2) {
-        V[k] = R[j];
-        j++;
-        k++;
-    }
-}
-
-void mergeSort(vector<int>& V, int l, int r) {
-    if (l < r) {
-        int m = l + (r - l) / 2;
-        mergeSort(V, l, m);
-        mergeSort(V, m + 1, r);
-        merge(V, l, m, r);
-    }
-}
-*/
 void call_registry::prin() const {
     print();
 }
